0df9ea3cdb7249f027a67df03808aae924a2def2
denismospanov <115163705+denismospanov@users.noreply.github.com>
2023-04-01 15:51:08 -0400

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -124,9 +124,6 @@ const Products = (props) => {
   let list = items.map((item, index) => {
     let n = index + 1049;
      let uhit = "http://picsum.photos/" + n;
-    // note, source.unsplash is used here because it loads images faster than picsum.photos
-    // it should functionally be the same as picsum.photos which is shown in the videos
-    //let uhit = "https://source.unsplash.com/random/800x800/?img=" + n;
 
     return (
       <li key={index}>
